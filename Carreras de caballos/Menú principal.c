d8283b32b55cb39f6337f65a231e7c4b1c25b84f
Luispdelmazo <79968606+Luispdelmazo@users.noreply.github.com>
2021-07-12 21:41:26 +0200

    Resgistro de partidas y ordenamiento puntuaciones

--- Carreras de caballos/Menú principal.c
+++ Carreras de caballos/Menú principal.c
@@ -8,27 +8,94 @@
 typedef struct
 {
     char nombre[50];
-    int posicion;
+    int puntuacion;
 }registro_jugadores;
 
-int main ()
+void ranking (registro_jugadores registro[], int dim);
+
+int main (int argc, char* argv[])
 {
-    int casos;
+    char instrucciones[]="Aqui instrucciones";
+    char aux;
+    int casos,nlineas=0,i=0;
     _Bool menu_on=1;
+    registro_jugadores *registro;
+
+    FILE *registro_partidas;
+
+    registro_partidas=fopen("Registro.txt","r");
+    fclose(registro_partidas);
 
 
     printf("Carrera de caballos\n\n");
 
     do
     {
-        printf("1.- Instrucciones\n2.- ")
+        printf("1.- Instrucciones\n2.- Jugar (2-4 personas)\n3.- Ver puntuaciones\n4.- Salir\n");
+        scanf("%i",&casos);
+
         switch (casos)
         {
         case 1:
+            printf("%s\n\n",instrucciones);
+            break;
+        case 2:
+            break;
+        case 3:
+            registro_partidas = fopen("Registro.txt","r");
+            while(fscanf(registro_partidas,"%c",&aux)!=EOF)
+                if (aux=='\n')
+                    nlineas++;
+
+            fseek(registro_partidas,0,SEEK_SET);
+            registro = malloc(sizeof(registro_jugadores)*nlineas);
+
+            if (registro==NULL)
+            {
+                printf("Error");
+                exit(-1);
+            }
+
+            while(fscanf(registro_partidas,"%[^:]: %i\n",registro[i].nombre,&registro[i].puntuacion)!=EOF)
+                i++;
+
+            ranking(registro,nlineas);
+            for (int i=0;i<nlineas;i++)
+                printf("%s: %i\n",registro[i].nombre,registro[i].puntuacion);
+            printf("\n");
+            fclose(registro_partidas);
+            break;
+        case 4:
+            break;
+        default:
+            break;
 
         }
 
     }while(menu_on);
 
+
     return 0;
+}
+
+void ranking (registro_jugadores registro[], int dim)
+{
+    int aux;
+    char name_aux[50];
+
+    for (int i=0;i<dim-1;i++)
+    {
+        for (int j=i+1;j<dim;j++)
+        {
+            if (registro[i].puntuacion<registro[j].puntuacion)
+            {
+                aux=registro[i].puntuacion;
+                strcpy(name_aux,registro[i].nombre);
+                registro[i].puntuacion=registro[j].puntuacion;
+                strcpy(registro[i].nombre,registro[j].nombre);
+                registro[j].puntuacion=aux;
+                strcpy(registro[j].nombre,name_aux);
+            }
+        }
+    }
 }
