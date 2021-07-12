51b2fd272653a8a4618cb3d95758e83c65b3c8a4
Luispdelmazo <79968606+Luispdelmazo@users.noreply.github.com>
2021-07-12 20:40:52 +0200

    Camino relativo + inicio del proyecto

--- Carreras de caballos/Menú principal.c
+++ Carreras de caballos/Menú principal.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+typedef struct
+{
+    char nombre[50];
+    int posicion;
+}registro_jugadores;
+
 int main ()
 {
+    int casos;
+    _Bool menu_on=1;
 
+
+    printf("Carrera de caballos\n\n");
+
+    do
+    {
+        printf("1.- Instrucciones\n2.- ")
+        switch (casos)
+        {
+        case 1:
+
+        }
+
+    }while(menu_on);
 
     return 0;
 }
